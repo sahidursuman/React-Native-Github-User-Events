99ac79f2e9b9047f0e2a5db9604dc8f1be6a0914
Julian Currie <julian.currie@gmail.com>
2016-10-12 21:45:34 -0700

    added random pikachu image to js files

--- index.ios.js
+++ index.ios.js
@@ -1,19 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   Text,
+  Image,
+  View
 } from 'react-native';
 
-export default class TestProject extends Component {
+class TestProject extends Component {
   render() {
+    let pic = {
+      uri: 'https://pbs.twimg.com/media/CS-IToTWoAA3Eiq.jpg'
+    };
     return (
-      <Text>Hello World!</Text>
+      <View>
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Text>Hello World!</Text>
+      </View>
     );
   }
 }
