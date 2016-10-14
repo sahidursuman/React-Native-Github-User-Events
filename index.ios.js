ee4a90032ae9218eac82287aaa3e7edfbd1e90e0
Julian Currie <julian.currie@gmail.com>
2016-10-14 01:03:05 -0700

    removed incorrect import

--- index.ios.js
+++ index.ios.js
@@ -2,6 +2,5 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native'
 import TestProject from './app/components'
 import Greeting from './app/components'
-import ViewContainer from './app/components'
 
 AppRegistry.registerComponent('TestProject', () => Greeting);
